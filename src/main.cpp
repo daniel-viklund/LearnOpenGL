c4adef445d69bacb8f7722f491d0abf3495b5753
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-06-11 15:02:56 +0200

    shader files

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <stdexcept>
 #include <iostream>
+#include "shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-std::string readShaderFile(const std::string& path);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -41,34 +37,16 @@ int main()
         return -1;
     }
 
-    std::string vertSrc  = readShaderFile("/Users/danielviklund/Documents/GitHub/LearnOpenGL/shaders/vertex.glsl");
-    std::string frag1Src = readShaderFile("/Users/danielviklund/Documents/GitHub/LearnOpenGL/shaders/fragment_blinking_green.glsl");
+    Shader shader = Shader("/Users/danielviklund/Documents/GitHub/LearnOpenGL/shaders/fragment.glsl", "/Users/danielviklund/Documents/GitHub/LearnOpenGL/shaders/vertex.glsl");
 
-    const char* vertexShaderSource   = vertSrc.c_str();
-    const char* fragmentShaderSource = frag1Src.c_str();
-
-    unsigned int vertexShader   = glCreateShader(GL_VERTEX_SHADER);
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    unsigned int shaderProgram  = glCreateProgram();
-
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    float triangle[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f,
+    // clang-format off
+    float vertices[] = {
+    // positions         // colors
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
     };
+    // clang-format on
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -76,9 +54,15 @@ int main()
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                          (void*)0);
     glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                          (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -87,13 +71,7 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
-
-        float timeValue  = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
+        shader.use();
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -103,7 +81,7 @@ int main()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
+    shader.deleteProgram();
 
     glfwTerminate();
     return 0;
@@ -124,15 +102,4 @@ void processInput(GLFWwindow* window)
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
-}
-
-std::string readShaderFile(const std::string& path)
-{
-    std::ifstream file(path);
-    if (!file.is_open())
-        throw std::runtime_error("Failed to open shader file: " + path);
-
-    std::ostringstream buf;
-    buf << file.rdbuf();
-    return buf.str();
 }
